4d1fe1469dc7ced9a2cb23890483597188ec07eb
nilemo <nicolas.leiva@bupa.cl>
2024-08-28 12:44:30 -0400

    session management

--- init.lua
+++ init.lua
@@ -220,6 +220,22 @@ require('lazy').setup({
     end,
   },
 
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    dependencies = {
+      'nvim-telescope/telescope.nvim', -- Only needed if you want to use session lens
+    },
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+  },
+
   -- NOTE: Plugins can specify dependencies.
   --
   -- The dependencies are proper plugin specifications as well - anything
